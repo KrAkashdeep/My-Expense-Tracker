96fa0076490d800eff9a34972828064bdfe0eb5f
KrAkashdeep <akashdeep93140@gmail.com>
2025-03-12 10:07:12 +0530

    add

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const API_URL = "https://expense-backend-vert.vercel.app";
-const API_URL = "http://localhost:3000";
+const API_URL = "https://expense-tracker-backend-steel.vercel.app";
 
 // Create an axios instance
 const api = axios.create({
